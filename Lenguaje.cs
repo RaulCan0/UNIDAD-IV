c8cc0987c9de288b7f565c63b8d15e62712b876a
[raulc] <[raulcanobriseno@gmail.com]>
2022-11-23 09:18:30 -0600

    actualizacion codigo en clase

--- Lenguaje.cs
+++ Lenguaje.cs
@@ -1,17 +1,106 @@
 //Alumno Raúl Cano Briseño
 using System;
 using System.Collections.Generic;
+//Requerimiento 1: Construir un metodo para escribir en el archivo lenguaje.cs identando el codigo
+//                 { incrementa un tabulador, } decrementa un tabulador
+//Requerimiento 2: Declarar un atributo primeraProduccion de tipo string y actualizarlo con la primera
+//                 produccion de la gramatica
+//Requerimiento 3: La primera produccion es publica y el resto privadas
 
 namespace GENERADOR
 {
     public class Lenguaje : Sintaxis, IDisposable
     {
-         public void Dispose()
+        string nombreProyecto;
+        public Lenguaje(string nombre) : base(nombre)
         {
-            cerrar(); 
-            Console.WriteLine("Destructor");
+            nombreProyecto = "";
         }
-       
-    }
+        public Lenguaje()
+        {
+            nombreProyecto = "";
+        }
+        public void Dispose()
+        {
+            cerrar();
+        }
+        private void Programa(string espacioProyecto, string produccionPrincipal)
+        {
+            programa.WriteLine("using System;");
+            programa.WriteLine("using System.IO;");
+            programa.WriteLine("using System.Collections.Generic;");
+            programa.WriteLine();
+            programa.WriteLine("namespace " + espacioProyecto);
+            programa.WriteLine("{");
+            programa.WriteLine("\tpublic class Program");
+            programa.WriteLine("\t{");
+            programa.WriteLine("\t\tstatic void Main(string[] args)");
+            programa.WriteLine("\t\t{");
+            programa.WriteLine("\t\t\ttry");
+            programa.WriteLine("\t\t\t{");
+            programa.WriteLine("\t\t\t\tusing (Lenguaje a = new Lenguaje())");
+            programa.WriteLine("\t\t\t\t{");
+            programa.WriteLine("\t\t\t\t\ta." + produccionPrincipal + "();");
+            programa.WriteLine("\t\t\t\t}");
+            programa.WriteLine("\t\t\t}");
+            programa.WriteLine("\t\t\tcatch (Exception e)");
+            programa.WriteLine("\t\t\t{");
+            programa.WriteLine("\t\t\t\tConsole.WriteLine(e.Message);");
+            programa.WriteLine("\t\t\t}");
+            programa.WriteLine("\t\t}");
+            programa.WriteLine("\t}");
+            programa.WriteLine("}");
+        }
+        public void gramatica()
+        {
+            cabecera();
+            Programa(nombreProyecto, "programa");
+            cabeceraLenguaje(nombreProyecto);
+            listaProducciones();
+            lenguaje.WriteLine("\t}");
+            lenguaje.WriteLine("}");
+        }
 
+        private void cabecera()
+        {
+            match("Gramatica");
+            match(":");
+            nombreProyecto = getContenido();
+            match(Tipos.SNT);
+            match(Tipos.FinProduccion);
+        }
+        private void cabeceraLenguaje(string espacioProyecto)
+        {
+            lenguaje.WriteLine("using System;");
+            lenguaje.WriteLine("using System.Collections.Generic;");
+            lenguaje.WriteLine("namespace "+espacioProyecto);
+            lenguaje.WriteLine("{");
+            lenguaje.WriteLine("\tpublic class Lenguaje : Sintaxis, IDisposable");
+            lenguaje.WriteLine("\t{");
+            lenguaje.WriteLine("\t\tstring nombreProyecto;");
+            lenguaje.WriteLine("\t\tpublic Lenguaje(string nombre) : base(nombre)");
+            lenguaje.WriteLine("\t\t{");
+            lenguaje.WriteLine("\t\t}");
+            lenguaje.WriteLine("\t\tpublic Lenguaje()");
+            lenguaje.WriteLine("\t\t{");
+            lenguaje.WriteLine("\t\t}");
+            lenguaje.WriteLine("\t\tpublic void Dispose()");
+            lenguaje.WriteLine("\t\t{");
+            lenguaje.WriteLine("\t\t\tcerrar();");
+            lenguaje.WriteLine("\t\t}");
+        }
+        private void listaProducciones()
+        {
+            lenguaje.WriteLine("\t\tprivate void "+getContenido()+"()");
+            lenguaje.WriteLine("\t\t{");
+            match(Tipos.SNT);
+            match(Tipos.Produce);
+            match(Tipos.FinProduccion);
+            lenguaje.WriteLine("\t\t}");
+            if (!FinArchivo())
+            {
+                listaProducciones();
+            }
+        }
+    }
 }
